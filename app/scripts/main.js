563eeb0b0696c172077301f88cbfcd6cdc15e049
BRodriguez90 <bero14@knights.ucf.edu>
2017-06-06 16:19:48 -0400

    Added fade animations/bg-color change

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+$(function(){
+
+    setTimeout(function(){
+        $('.my-intro > p').fadeIn(1000);
+        $('.my-silh').fadeIn(1000);
+    },200);
+
+
+
+
+
+ });

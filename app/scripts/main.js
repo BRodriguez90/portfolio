f4a82e3d3cc028999e518280d46b9618b112d342
BRodriguez90 <bero14@knights.ucf.edu>
2017-06-30 19:24:21 -0400

    menu started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,9 +5,12 @@ $(function(){
         $('.my-intro > p').fadeIn(1000);
         $('.my-silh').fadeIn(1000);
     },200);
-
-
-
-
-
+   
+    $("#nav-button").click(function(){
+        $(this).toggleClass('clicked');
+        $("#nav-button.clicked:focus").css("outline","none");
+        $(".line2").toggleClass("line-tog");
+        $("nav#overlay-nav").slideToggle(650);
+       
+    });
  });

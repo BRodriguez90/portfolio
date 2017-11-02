3a4981c570d494cd307dc47bd705ac48c6face05
BRodriguez90 <bero14@knights.ucf.edu>
2017-11-02 16:12:08 -0400

    Menu Updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,8 +9,11 @@ $(function(){
     $("#nav-button").click(function(){
         $(this).toggleClass('clicked');
         $("#nav-button.clicked:focus").css("outline","none");
-        $(".line2").toggleClass("line-tog");
+        $(".line1,.line2,.line3").toggleClass("line-tog");
         $("nav#overlay-nav").slideToggle(650);
-       
     });
+    setTimeout(function(){
+        //$(".my-silh embed").css("background-color","#e6e6e6");
+    },6000);
+    
  });

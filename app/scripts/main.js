dc933f838d88ea2d7c43a7157fb57129d5ee8c7d
BRodriguez90 <bero14@knights.ucf.edu>
2017-12-28 17:02:58 -0500

    work section updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,39 @@
 console.log('\'Allo \'Allo!');
-$(function(){
+$(function() {
+    /*$('.my-intro').textillate({ in: { 
+        effect: 'fadeIn' 
+        } 
+    });*/
+    var i = 0;
+    var txt = 'I design things up and code them down.'; //Text
+    var speed = 65; //Duration
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("sub-p").innerHTML += txt.charAt(i);
+       
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+    $(".tags li").addClass('s_tags');
 
     setTimeout(function(){
-        $('.my-intro > p').fadeIn(1000);
-        $('.my-silh').fadeIn(1000);
+        //$('.my-intro p').fadeIn(1000);
+        //$('.my-intro p').css('display','block');
+        //$('.my-intro p:first-child').animateCss('fadeIn');
+       // $('.my-silh').fadeIn(1000);
     },200);
+    
    
-    $("#nav-button").click(function(){
+    $('#nav-button').click(function(){
         $(this).toggleClass('clicked');
-        $("#nav-button.clicked:focus").css("outline","none");
-        $(".line1,.line2,.line3").toggleClass("line-tog");
-        $("nav#overlay-nav").slideToggle(650);
+        $('#nav-button.clicked:focus').css('outline','none');
+        $('.line1,.line2,.line3').toggleClass('line-tog');
+        $('nav#overlay-nav').slideToggle(650);
     });
     setTimeout(function(){
         //$(".my-silh embed").css("background-color","#e6e6e6");
     },6000);
-   
+   window.addEventListener("load",typeWriter);
  });

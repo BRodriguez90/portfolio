eeb20931b6edc879669e0581495216735f7b3bc0
BRodriguez90 <bero14@knights.ucf.edu>
2018-01-21 12:37:19 -0500

    minor style changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,6 @@ $(function() {
     function typeWriter() {
       if (i < txt.length) {
         document.getElementById('sub-p').innerHTML += txt.charAt(i);
-       
         i++;
         setTimeout(typeWriter, speed);
       }

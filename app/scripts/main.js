629e93aa935d786720e92c24db5c7fbceaaf1fba
BRodriguez90 <bero14@knights.ucf.edu>
2018-02-05 15:59:23 -0500

    changed mobile nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,24 +17,54 @@ $(function() {
     }
     $('.tags li').addClass('s_tags');
 
-    setTimeout(function(){
-        //$('.my-intro p').fadeIn(1000);
-        //$('.my-intro p').css('display','block');
-        //$('.my-intro p:first-child').animateCss('fadeIn');
-       // $('.my-silh').fadeIn(1000);
-    },200);
+    /*setTimeout(function(){
+        $('.my-intro p').fadeIn(1000);
+        $('.my-intro p').css('display','block');
+        $('.my-intro p:first-child').animateCss('fadeIn');
+        $('.my-silh').fadeIn(1000);
+    },200);*/
     
    
-    $('#nav-button').click(function(){
-        $(this).toggleClass('clicked');
+    $('#nav-button').click(function() {
+        $('#nav-button').toggleClass('clicked');
         $('#nav-button.clicked:focus').css('outline','none');
-        $('.line1,.line2,.line3').toggleClass('line-tog');
-        $('nav#overlay-nav').slideToggle(650);
+       // $('.line1,.line2,.line3').toggleClass('line-tog');
+       
+       $('nav#mobile-nav ul').slideToggle(700);
+        
+          
+       //$('nav#mobile-nav ul').toggleClass('show-ul');
+        
     });
-    setTimeout(function(){
-        //$(".my-silh embed").css("background-color","#e6e6e6");
-    },6000);
+    $('nav#mobile-nav li a').click(function(){
+      $('#nav-button').toggleClass('clicked');
+      $('nav#mobile-nav ul').slideToggle(700);
+    });
 
     
    window.addEventListener('load',typeWriter);
+   
+     // Add smooth scrolling to all links
+  $('a').on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
  });
